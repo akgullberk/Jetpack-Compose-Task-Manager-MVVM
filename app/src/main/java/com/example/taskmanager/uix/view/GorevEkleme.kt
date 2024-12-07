eddb8840a17ea2aa507ca5f129ee717d911f448f
Berk Akgül <berkak_gul51@hotmail.com>
2024-12-07 17:40:59 +0300

    custom textfield

--- app/src/main/java/com/example/taskmanager/uix/view/GorevEkleme.kt
+++ app/src/main/java/com/example/taskmanager/uix/view/GorevEkleme.kt
@@ -1,22 +1,35 @@
 package com.example.taskmanager.uix.view
 
+import android.content.res.Resources
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
@@ -27,12 +40,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.taskmanager.R
 import com.example.taskmanager.uix.viewmodel.GorevEklemeViewModel
@@ -47,6 +69,8 @@ import java.time.LocalDate
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GorevEkleme(navController: NavController, gorevEklemeViewModel: GorevEklemeViewModel){
+
+
     val tfGorevAd = remember {
         mutableStateOf("")
     }
@@ -55,7 +79,20 @@ fun GorevEkleme(navController: NavController, gorevEklemeViewModel: GorevEklemeV
         mutableStateOf("")
     }
 
+
+
     val calendarState = rememberSheetState()
+
+    val textState = remember { mutableStateOf("") }
+
+    // Text'in stilini belirleme
+    val textStyle = TextStyle(fontSize = 16.sp)
+
+    // TextHeight'i dinamik olarak güncellemek için
+    val textHeight = remember { mutableStateOf(0.dp) }
+
+    // LocalDensity ile piksel ölçümlerine dönüştürme işlemi
+    val density = LocalDensity.current
 
     CalendarDialog(
         state = calendarState,
@@ -74,6 +111,10 @@ fun GorevEkleme(navController: NavController, gorevEklemeViewModel: GorevEklemeV
         navController.navigate("anasayfa")
         gorevEklemeViewModel.kaydet(tfGorevAd.value,cdGorevTarihi.value)
 
+    }
+
+    fun Int.toDp(): Dp {
+        return (this / Resources.getSystem().displayMetrics.density).dp
     }
 
     Scaffold(
@@ -110,43 +151,81 @@ fun GorevEkleme(navController: NavController, gorevEklemeViewModel: GorevEklemeV
         Box(modifier = Modifier
             .padding(paddingValues)
             .fillMaxSize()
+            .verticalScroll(rememberScrollState()) // Kaydırma işlemi burada sağlanıyor
             .background(Color(0xFF8AA6A3))
         ) {
+
 
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(15.dp)
-
-
             ) {
                 Text(
                     text = "Ne Yapılacak?", color = Color(0xFF127369), fontWeight = FontWeight.Bold,)
-                TextField(
+
+
+
+                BasicTextField(
                     value = tfGorevAd.value,
                     onValueChange = {tfGorevAd.value = it},
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    label = { Text(text = "Buraya görevi girin", color = Color.White)},
-                    colors = TextFieldDefaults.textFieldColors(
-                        containerColor = Color.Transparent,
-                        focusedLabelColor = Color.Black,
-                        focusedIndicatorColor = Color(0xFF127369),
-                        unfocusedLabelColor = Color.Black,
-                        unfocusedIndicatorColor = Color(0xFF127369),
-                    ))
-                
-                Button(onClick = {
-                    calendarState.show()
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .heightIn(min = 50.dp)
+                                .background(Color.Transparent)
+                                .drawBehind {
+                                    val strokeWidth = 1.dp.toPx()
+                                    val y = size.height - strokeWidth / 2
+                                    drawLine(
+                                        color = Color(0xFF127369),
+                                        start = Offset(0f, y),
+                                        end = Offset(size.width, y),
+                                        strokeWidth = strokeWidth
+                                    )
+                                }
+                                .onGloballyPositioned { coordinates ->
+                                    // Metnin yüksekliğini hesaplamak için
+                                    val height = with(density) {
+                                        // Text'in satır sayısını alıp, yükseklik hesaplaması yapıyoruz
+                                        textStyle.fontSize.toPx() * textState.value.lines().size
+                                    }
+                                    textHeight.value = height.dp
+                                },
 
-                }) {
+                    textStyle = TextStyle(
+                        fontSize = 20.sp
+
+
+                    ),
+                    singleLine = false, // Çok satırlı olmasını sağlıyoruz,
+                    decorationBox = { innerTextField ->
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.BottomStart // Varsayılan hizalama
+                        ) {
+
+                            if (tfGorevAd.value.isEmpty()) {
+                                // Boşsa "Hint" göster
+                                Text(
+                                    text = "Buraya görevi girin",
+                                    color = Color.DarkGray,
+                                    fontSize = 16.sp,
+                                )
+                            }
+
+                            innerTextField() // Gerçek metin alanı
+                        }
+                    },
+
+                )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                    ,onClick = { calendarState.show() }) {
 
                 }
-
             }
         }
-
-
-
     }
 }
+
